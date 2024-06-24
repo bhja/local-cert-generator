7aad8d47b01927f94590e8fe8bf1dff2511b2f61
unknown <bh.ja.699@gmail.com>
2024-06-24 10:48:26 +0000

    code updates for windows

--- createRootCA.sh
+++ createRootCA.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-openssl genrsa -des3 -out rootCA.key 2048
-openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 7300 -out rootCA.pem
+winpty openssl genrsa -des3 -out rootCA.key 2048
+winpty openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 7300 -out rootCA.pem
